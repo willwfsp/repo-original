1565e46c0bf5ca1249639344488280749b4ae538
mrtakata <matheustakata@Matheuss-MacBook-Pro.local>
2015-08-10 12:29:16 -0300

    committee members table working with ngTable

--- master/js/custom/controllers/committees.js
+++ master/js/custom/controllers/committees.js
@@ -6,20 +6,29 @@
  myApp.controller('CommitteesController',
   ['$scope', '$log', '$stateParams', '$filter', 'ngTableParams', 'DataFetcher',
     function($scope, $log, $stateParams, $filter, ngTableParams, DataFetcher){
-
     $scope.committeesMembers = [];
+    $scope.membersJson = [];
 
     $scope.membersTableParams = new ngTableParams({
             page: 1,
-            count: 10
+            count: 10,
+            sorting: {name: 'asc'}
         }, {
-            total: $scope.committeesMembers.length,
+            total: $scope.membersJson.length,
             counts: [],
             getData: function($defer, params){
-                console.log(params);
+
+                var filteredData = $scope.membersJson;
+                if (params.filter() ){
+                    orderedData = $filter('filter')($scope.membersJson, params.filter());
+                }
+
                 var orderedData = params.sorting() ?
-                    $filter('orderBy')($scope.committeesMembers, params.orderBy()) :
-                    $scope.committeesMembers;
+                    $filter('orderBy')(filteredData, params.orderBy()) :
+                    filteredData;
+
+                params.total(orderedData.length); // set total for recalc pagination
+                console.log(orderedData.length);
                 $defer.resolve(orderedData);
             }
     });
@@ -28,6 +37,17 @@
         $stateParams.committeeID).then(function(data) {
         $scope.committeesDetails = data[0].data;
         $scope.committeesMembers = data[1].data.rows;
+
+        for(var i = 0; i < $scope.committeesMembers.length; i++){
+            var parsedMember = {};
+            parsedMember.name = $scope.committeesMembers[i].key[3];
+            parsedMember.position = $scope.committeesMembers[i].key[4];
+            parsedMember.situation = $scope.committeesMembers[i].key[5];
+            parsedMember.party = $scope.committeesMembers[i].key[6];
+            parsedMember.house = $scope.committeesMembers[i].key[0];
+            parsedMember.id = $scope.committeesMembers[i].key[8];
+            $scope.membersJson.push(parsedMember);
+        };
         $scope.membersTableParams.reload();
     });
 }]);
