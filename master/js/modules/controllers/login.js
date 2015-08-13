2dea3c411e0f23849e2af8948f114fefda4122a5
Danilo Oliveira <daniloa.oliveira@gmail.com>
2015-08-13 19:50:52 -0300

    Commit Segurança

--- master/js/modules/controllers/login.js
+++ master/js/modules/controllers/login.js
@@ -16,15 +16,13 @@ App.controller('LoginFormController', ['$scope', '$http', '$state', function($sc
     if($scope.loginForm.$valid) {
 
       $http
-        .post('http://localhost:6005/rest/login', {login: $scope.loginUser, password: $scope.account.password})
+        .post('https://sigalei-api.mybluemix.net/v1/accounts/login', {login: $scope.loginUser, password: $scope.account.password})
         .then(function(response) {
           // assumes if ok, response is an object with some data, if not, a string with error
           // customize according to your api
-          if ( response.status == "204" ) {
-            $state.go('app.dashBoard');
-          }
+          alert(response);
         }, function(x) {
-          $scope.authMsg = 'Server Request Error';
+          $scope.authMsg = 'Usuário/Senha incorretos';
         });
     }
     else {
